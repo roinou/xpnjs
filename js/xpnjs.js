56c55ed5780efcb82dec3c31eeee5bcf14b104ce
roinou <erwan.lacoste+github@gmail.com>
2014-04-23 18:12:10 +0200

    work on line adding

--- js/xpnjs.js
+++ js/xpnjs.js
@@ -6,48 +6,42 @@
 var xpnjsApp = angular.module('xpnjsApp', []);
 
 xpnjsApp.controller('xpnsListCtrl', function ($scope, $http) {
+    // debug?
+    $scope.debug = true;
+
     // loading data
     $http.get('data/xpns-201312.json').success(function(data) {
         $scope.xpns = data.data;
+
+        // next line ID
+        $scope.nextId = $scope.xpns.length + 1;
     });
+    // property on which to order by
     $scope.orderProp = 'date';
 
-    // sum all data
-    /*$scope.totalResto = 0;
-    $scope.totalParking = 0;
-    function calcTotal($scope) {
-        angular.forEach($scope.xpns, function(xpn) {
-            console.log(xpn);
-            $scope.totalResto += xpn.resto;
-            $scope.totalParking += xpn.parking;
-        });
-    };*/
-    $scope.$watch('xpns', function(records) {
-        $scope.totalResto = 0;
-        $scope.totalParking = 0;
+    // watching changes to recalculate total amounts
+    $scope.$watchCollection('xpns', function(records) {
+        var resto = 0;
+        var parking = 0;
         angular.forEach(records, function(record) {
-            $scope.totalResto += record.resto;
-            $scope.totalParking += record.parking;
+            if (record.resto) resto += record.resto;
+            if (record.parking) parking += record.parking;
         });
+        $scope.totalParking = parking;
+        $scope.totalResto = resto;
+        $scope.total = parking + resto;
     });
 
 
-
-    // new data form:
-    $scope.newDate = '';
-    $scope.newLabel = '';
-    $scope.newResto = 0;
-    $scope.newParking = 0;
-
-    $scope.addLine = function() {
-        var newLine = new function() {
-            this.date = $scope.newDate;
-            this.label = $scope.newLabel;
-            this.resto = $scope.resto;
-        };
-        console.log('adding: ' + newLine);
-        $scope.xpns.push(newLine);
-    }
+    $scope.addLine = function(newLine, valid) {
+        if (valid) {
+            console.log('adding: ' + newLine);
+            newLine.id = $scope.nextId;
+            $scope.nextId++;
+            $scope.xpns.push(newLine);
+            $scope.newLine = null;
+        }
+    };
 
     // edit line
 

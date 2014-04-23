5071aa0e03733221dd9e6bbe2232b419a2be4ec7
roinou <erwan.lacoste+github@gmail.com>
2014-04-23 20:07:47 +0200

    work on line editing

--- js/xpnjs.js
+++ js/xpnjs.js
@@ -3,7 +3,7 @@
  */
 'use strict';
 
-var xpnjsApp = angular.module('xpnjsApp', []);
+var xpnjsApp = angular.module('xpnjsApp', ["xeditable"]);
 
 xpnjsApp.controller('xpnsListCtrl', function ($scope, $http) {
     // debug?
@@ -20,7 +20,8 @@ xpnjsApp.controller('xpnsListCtrl', function ($scope, $http) {
     $scope.orderProp = 'date';
 
     // watching changes to recalculate total amounts
-    $scope.$watchCollection('xpns', function(records) {
+    $scope.$watch('xpns', sumLines, true);
+    function sumLines(records) {
         var resto = 0;
         var parking = 0;
         angular.forEach(records, function(record) {
@@ -30,20 +31,50 @@ xpnjsApp.controller('xpnsListCtrl', function ($scope, $http) {
         $scope.totalParking = parking;
         $scope.totalResto = resto;
         $scope.total = parking + resto;
-    });
+    }
 
-
+    // adding OR editing a line
     $scope.addLine = function(newLine, valid) {
-        if (valid) {
-            console.log('adding: ' + newLine);
-            newLine.id = $scope.nextId;
-            $scope.nextId++;
+        if (valid ) {
+            if (newLine && !newLine.id) {
+                console.log('adding: ' + newLine);
+                newLine.id = $scope.nextId;
+                $scope.nextId++;
+            } else {
+                console.log('replacing: ' + newLine);
+                removeById($scope.xpns, newLine.id);
+            }
             $scope.xpns.push(newLine);
             $scope.newLine = null;
         }
     };
 
     // edit line
+    $scope.editLine = function(line) {
+        $scope.newLine = angular.copy(line);
+    }
 
     // delete line
+    $scope.removeLine = function (line) {
+        removeById($scope.xpns, line.id);
+    }
+
+    // utility methods
+    /**
+     * removes an element from the array by its ID
+     * @param array the array to look in
+     * @param id the id to find
+     * @returns {*} the element found
+     */
+    function removeById(array, id) {
+        for (var d = 0, len = array.length; d < len; d += 1) {
+            if(array[d].id === id)
+                return array.splice(d, 1);
+        }
+    }
+});
+
+// module init
+xpnjsApp.run(function(editableOptions) {
+    editableOptions.theme = 'bs3';
 });
